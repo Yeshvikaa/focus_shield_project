758d7293566ffe77a3a182143c12ceecdb0a3fd3
Yeshvikaa <yeshuprojects@gmail.com>
2026-06-03 07:27:02 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create instance pointing to backend server
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://focus-shield-project.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
